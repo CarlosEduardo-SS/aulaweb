c74684721ed33df404dc77bbd9929922faff998e
carlos <carloseduardosilvasantos2008@gmail.com>
2026-06-10 20:45:11 -0300

    main2 save

--- src/main/java/com/cursojava/aulaweb/PaginaController.java
+++ src/main/java/com/cursojava/aulaweb/PaginaController.java
@@ -6,23 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PaginaController {
     
-    @GetMapping("/sobre")
-    public String sobre() {
-        return "sobre";
+    @GetMapping("/exemplo")
+    public String exemplo() {
+        return "exemplo";
     }
 
-     @GetMapping("/contato")
-    public String contato() {
-        return "contato";
-    }
-
-     @GetMapping("/ajuda")
-    public String ajuda() {
-        return "ajuda";
+    @GetMapping("/ola")
+    public String ola() {
+        return "ola";
     }
 
     @GetMapping("/")
-    public String Index() {
-        return "index";
+    public String homePage() {
+        return "home_page";
     }
 }
